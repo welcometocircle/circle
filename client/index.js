7111ee637eb3438b7a05b7140e064cc4431a947b
matrix303 <maitray303@gmail.com>
2020-03-29 17:58:09 -0400

    working favicon

--- client/index.js
+++ client/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { HashRouter as Router } from "react-router-dom";
-
+import Favicon from 'react-favicon';
 
 
 ReactDOM.render(
     <Router>
       <App />
     </Router>,
+    
     document.getElementById('root')
   );
 
