d5a2b3fec0c1583e9194b67887235237f22887e3
Harsh Patel <contact@harshpatel.ca>
2020-06-07 15:38:10 -0400

    animated compass logo and added coming soon

--- client/components/Compass.js
+++ client/components/Compass.js
@@ -15,12 +15,16 @@ class Compass extends Component {
                 <div className="compass-background">
                     <div className="compass container">
                         <div className="row justify-content-center">
-                            <img src="assets/compass-logo.png" className="compass-logo col-lg-2 col-6"/>
+                            <img src="assets/compass-logo.gif" className="compass-logo col-lg-2 col-6"/>
                         </div>
                         <h2 className="compass-title">compass</h2>
                         <h3 className="compass-subtitle">a virtual hackathon like no other</h3>
+                    </div>
+                    <div className="compass-shape-div">
                         <p className="compass-desc">coming soon</p>
-                        
+                        <div className="row justify-content-center compass-shape"> 
+                            <img src="assets/compass-shape.png" className=""/>
+                        </div>
                     </div>
 
                 </div>
