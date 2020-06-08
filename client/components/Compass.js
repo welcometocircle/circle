e5e4491f2e4fb2f81bb7fbaaf71a0763e1e263cd
Harsh Patel <contact@harshpatel.ca>
2020-06-07 22:47:09 -0400

    Update Compass.js

--- client/components/Compass.js
+++ client/components/Compass.js
@@ -35,7 +35,7 @@ class Compass extends Component {
                 <div className="compass-background">
                     <div className="compass container">
                         <div className="row justify-content-center">
-                            <Lottie isClickToPauseDisabled options={aniCompass} height={500} width={500}/>
+                            <Lottie isClickToPauseDisabled options={aniCompass} height={200} width={200}/>
                         </div>
                         <h2 className="compass-title">compass</h2>
                         <h3 className="compass-subtitle">a virtual hackathon like no other</h3>
