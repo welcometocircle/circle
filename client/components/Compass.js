f46d45f944923e1f86511a08065a86ac2b14bb75
matrix303 <maitray303@gmail.com>
2020-06-07 20:37:54 -0400

    compass logo animation

--- client/components/Compass.js
+++ client/components/Compass.js
@@ -1,21 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, createRef, useEffect } from 'react';
 import '../css/compass.css';
+import Lottie from "react-lottie";
+import * as aniCompassData from "../assets/compass-rotating-logo/compass-logo-layered.json"
 
 
 
 class Compass extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isStopped: false,
+            isPaused: false
+        };
+    }
+
     componentDidMount() {
         window.scrollTo(0, 0);
         analytics.logEvent("Compass Did Mount")
     }
 
     render() {
+
+        const aniCompass  = {
+                animationData: aniCompassData.default,
+                loop: true,
+                autoplay: true,
+                assetsPath: "../assets/compass-rotating-logo/images/",
+                rendererSettings:{
+                    preserveAspectRatio: 'xMidYMid slice'
+                }
+        };
         return (
             <div>
                 <div className="compass-background">
                     <div className="compass container">
                         <div className="row justify-content-center">
-                            <img src="assets/compass-logo.gif" className="compass-logo col-lg-2 col-6"/>
+                            <Lottie options={aniCompass} height={500} width={500}/>
                         </div>
                         <h2 className="compass-title">compass</h2>
                         <h3 className="compass-subtitle">a virtual hackathon like no other</h3>
