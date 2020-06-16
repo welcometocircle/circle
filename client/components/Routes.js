72e1057a5fc523490102de1e4f999a98b459f029
matrix303 <maitray303@gmail.com>
2020-06-16 12:21:22 -0400

    updated link to sponspkg

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -4,7 +4,7 @@ import Home from "./Home";
 import Opportunities from "./Opportunities";
 import Faqs from "./Faqs";
 import Compass from "./Compass";
-
+import SponsPkg from "./SponsPkg"
 export default function Routes() {
   return (
     <Switch>
@@ -12,6 +12,7 @@ export default function Routes() {
       <Route path="/opportunities" exact component={Opportunities} />
       <Route path="/faqs" exact component={Faqs} />
       <Route path="/compass" exact component={Compass} />
+      <Route path="/sponspkg" exact component = {SponsPkg}/>
     </Switch>
   );
 }
