7c201b9477b527938ae8d5f5bffba5e5cf231ea7
Harsh Patel <contact@harshpatel.ca>
2020-06-27 14:31:53 -0400

    added speaker company logo temp

--- client/components/Compass/CSponsor.js
+++ client/components/Compass/CSponsor.js
@@ -15,7 +15,7 @@ class CSponsor extends Component {
     return (
       <div className="col-lg-3 col-11 mb-lg-0 mb-5 compass-sponsor">
         <div className="row justify-content-center mt-5">
-          <img src={this.props.src} className="compass-sponsor-img col-8" />
+          <img src={this.props.src} className="compass-sponsor-img col-10" />
         </div>
         <h5 className="compass-h5 mt-5">{this.props.name}</h5>
         <p className="compass-desc mt-2">{this.props.title}</p>
