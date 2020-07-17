7cb653c3d3530746e3809d17a783feee876d3dec
matrix303 <maitray303@gmail.com>
2020-07-17 01:00:48 -0400

    timer working

--- client/components/Compass/CompassTimerEventPage.js
+++ client/components/Compass/CompassTimerEventPage.js
@@ -7,15 +7,31 @@ class CompassTimerEventPage extends Component {
   constructor(props) {
     super(props);
     document.documentElement.setAttribute("data-theme", "dark");
+    this.state = {viewEventPage:false};
   }
 
   componentDidMount() {
     window.scrollTo(0, 0);
     analytics.logEvent("Event Did Mount");
+    console.log("MOUNT")
+    
+    if (this.state.viewEventPage == false) {
+      setInterval(function(){
+        console.log("interval")
+        const eventDate = new Date("2020-07-18T08:45:00-04:00")
+          console.log("FALSE")
+          if (Date.now()>eventDate){
+            window.location.reload(true)
+          }
+        },5000);
+    }
+
   }
 
+
+
   render() {
-    const eventDate = new Date("2020-07-18T08:45:00-04:00");
+    const eventDate = new Date("2020-07-18T08:45:00-04:00")
     // console.log(Date.now())
     // console.log(eventDate)
     // console.log(Date.now() > eventDate)
@@ -24,6 +40,7 @@ class CompassTimerEventPage extends Component {
         <CompassEventHolderPage/>
       )
     }else{
+      this.state.viewEventPage=true;
       return(
         <CompassEventPage/>
       )
