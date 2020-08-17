6b20dee9b45b45b0edad95166bac6ebf89f7c11f
matrix303 <maitray303@gmail.com>
2020-08-17 01:11:01 -0400

    innovation + lines

--- client/components/PLanding.js
+++ client/components/PLanding.js
@@ -5,25 +5,74 @@ import LottieAnimation from "./global/LottieAnimation";
 class PLanding extends Component {
   constructor(props) {
     super(props)
+    this.state = { width: window.innerWidth};
     document.documentElement.setAttribute("data-theme", "dark");
   }
   componentDidMount() {
     window.scrollTo(0, 0);
     analytics.logEvent("Landing Did Mount");
+
+    var aniDiv = document.getElementById('aniDivLanding');
+    var aniDivText = document.getElementById('landingText')
+    var isMouseOver = false 
+    var x;
+    var y
+
+    function transformAnimation (e) {
+      var xPos = (e.clientX - aniDiv.offsetWidth/2)/40
+      var yPos = (e.clientY - aniDiv.offsetHeight/2)/40
+      var translate3dValue = "translate3d("+xPos+'px,'+yPos+'px,0)';
+      aniDiv.style.transform = translate3dValue;
+    }
+
+    aniDivText.addEventListener("mousemove", transformAnimation, true);
+    aniDiv.addEventListener("mousemove", transformAnimation, true);
+    window.addEventListener("resize",this.handleResize.bind(this));
   }
+
+
+  handleResize (e) {
+    console.log('resize')
+    this.setState({width:window.innerWidth})
+  };
+
+  componentWillUnmount(){
+  }
+
+
   render() {
 
+    var aniHeight;
+    var aniWidth;
+    var aniMargin;
+    var txtMargin;
+
+    if (this.state.width<576){
+      console.log("576")
+      aniHeight = "130vh"
+      aniWidth =  "130vw"
+      aniMargin =  "-40vh"
+      txtMargin = "22vh"
+    } else if (this.state.width<1100){
+      console.log('768')
+      aniHeight = "130vh";
+      aniWidth = "130vw";
+      aniMargin = "-40vh";
+      txtMargin = "20vh"
+    } else{
+      console.log('large')
+      aniHeight = "130vh"
+      aniWidth = "130vw"
+      aniMargin = "-20vh"
+      txtMargin = "30vh"
+    }
+
     return (
-      <div>
-        <h1>Circle v2 -- Hype things aaa</h1>
-        <h2> h2 Circle v2 -- Hype things aaa</h2>
-        <h3> h3 Circle v2 -- Hype things aaa</h3>
-        <h4>h4 Circle v2 -- Hype things aaa</h4>
-        <h5 style={{ color: "#969696" }}>h5 Circle v2 -- Hype things aaa</h5>
-        <h6>h6 SECTORS</h6>
-        <hr />
-        <p>Test aaa asjdofajlfk alsdf alksdf </p>
-        <LottieAnimation aniData={aniLinesData}/>
+      <div className="container">
+        <div className="row justify-content-center" style={{position:"relative", zIndex:0}}>
+            <h1 id="landingText" style={{zIndex: 2,position:"absolute",marginTop: txtMargin,overflowX: 'hidden'}} className=''>Innovation is just<br/> the beginning.</h1>
+            <LottieAnimation aniData={aniLinesData} id="aniDivLanding" height={aniHeight} width={aniWidth} style={{zIndex: 1,position: "relative",marginTop:aniMargin}}/>
+        </div>
       </div>
     );
   }
--- client/components/global/LottieAnimation.js
+++ client/components/global/LottieAnimation.js
@@ -3,34 +3,76 @@ import Lottie from "react-lottie";
 
 class LottieAnimation extends Component{
   render() {
-    const aniCompass = {
-      animationData: {this.props.aniData}.default,
-      loop: true,
-      autoplay: true,
-      renderer: "canvas",
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid meet",
-        // progressiveLoad:true,
-        // clearCanvas:true,
-      },
-    };
+    // const aniCompass = {
+    //   animationData: {this.props.aniData}.default,
+    //   loop: true,
+    //   autoplay: true,
+    //   renderer: "canvas",
+    //   rendererSettings: {
+    //     preserveAspectRatio: "xMidYMid meet",
+    //     // progressiveLoad:true,
+    //     // clearCanvas:true,
+    //   },
+    // };
+
+
+
+    if (this.props.loop === undefined) {
+      var loop = true;
+    } else {
+      var loop = this.props.loop;
+    }
+
+    if (this.props.autoplay === undefined) {
+      var autoplay = true;
+    } else {
+      var autoplay = this.props.autoplay;
+    }
+
+    if (this.props.isClickToPauseDisabled === undefined) {
+      var isClickToPauseDisabled = true;
+    } else {
+      var isClickToPauseDisabled = this.props.isClickToPauseDisabled;
+    }
+
+    if (this.props.renderer === undefined) {
+      var renderer = "canvas";
+    } else {
+      var renderer = this.props.renderer;
+    }
+
+    if (this.props.preserveAspectRatio === undefined) {
+      var preserveAspectRatio = "xMidYMid meet";
+    } else {
+      var preserveAspectRatio = this.props.preserveAspectRatio;
+    }
+
+    if (this.props.renderer != "canvas" && this.props.renderer) {
+      renderer = this.props.renderer
+    }
+    if (this.props.preserveAspectRatio != "xMidYMid meet") {
+
+    }
+    
+    
+    console.log(loop);
     return (
-    <div className="row justify-content-center">
-              <Lottie
-                isClickToPauseDisabled
-                options={
-                    animationData: this.props.aniData.default,
-                    loop: true,
-                    autoplay: true,
-                    renderer: "canvas",
-                    rendererSettings: {
-                      preserveAspectRatio: "xMidYMid meet",
-                    }
-                  }
-                height={200}
-                width={200}
-              />
-    </div>
+      <div className="" style={this.props.style} id={this.props.id}>
+        <Lottie
+          isClickToPauseDisabled={isClickToPauseDisabled}
+          options={{
+            animationData: this.props.aniData.default,
+            loop: loop,
+            autoplay: autoplay ,
+            renderer: renderer,
+            rendererSettings: {
+              preserveAspectRatio: preserveAspectRatio,
+            },
+          }}
+          height={this.props.height}
+          width={this.props.width}
+        />
+      </div>
     );
   }
 }
