dd2ef8a90da78654ddf78c4820e4159d0707c3ac
matrix303 <maitray303@gmail.com>
2020-09-01 19:14:07 -0400

    implement mdbNav basic

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,41 +1,62 @@
 import React, { Component } from "react";
+import { MDBContainer, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBIcon } from 'mdbreact';
+import { BrowserRouter as Router } from "react-router-dom";
 
-const sNB = {
-  backgroundColor: "white",
-  // borderRadius: 25,
-  height: "100%",
-  textAlign: "center",
+const styles = {
+  navBG: {
+    backgroundColor: "#1F2022",
+    opacity: "1",
+  },
 };
 
-const sNBnav = {
-  color: "black",
-  fontWeight: 300,
-  textAlign: "center",
-  marginBottom: 15,
-};
-
-const descStyle = {
-  color: "black",
-  marginBottom: 30,
-};
-
-const imgStyle = {
-  borderRadius: 50,
-  height: "50%",
-  marginBottom: 15,
-};
 
 class NavBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      collapse: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
   }
 
   render() {
     return (
-      <div style={sectorStyle}>
-        <img style={imgStyle} src={this.props.img} />
-        <h6 style={titleStyle}>{this.props.title}</h6>
-        <p style={descStyle}>{this.props.desc}</p>
+      <div>
+        <Router>
+          <header>
+            <MDBNavbar style={styles.navBG} dark expand="lg" scrolling fixed="top">
+              <MDBNavbarBrand href="/">
+                <strong>circle</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={this.onClick} />
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav right>
+                  <MDBNavItem >
+                    <MDBNavLink to="#">About</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Open Circle</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Conferences</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Opportunities</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Contact</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+        </Router>
       </div>
     );
   }
