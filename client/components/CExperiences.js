562a8d7321506c4bbfab7d8dabcdf8cce48d900f
arattansi1 <rattansi.abbas@gmail.com>
2020-09-01 02:17:08 -0400

    add first few componenets

---
+++ client/components/CExperiences.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+const sectorStyle = {
+    backgroundColor: 'white',
+    // borderRadius: 25,
+    height: '100%',
+    textAlign: "center",
+};
+
+const titleStyle = {
+    color: 'black',
+    fontWeight: 300,
+    textAlign: 'center',
+    marginBottom: 15,
+};
+
+const descStyle = {
+    color: 'black',
+    marginBottom: 30,
+}
+
+const imgStyle = {
+    borderRadius: 50,
+    height: '50%',
+    marginBottom: 15,
+
+}
+
+class CExperiences extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div style={sectorStyle}>
+                <img style={imgStyle} src={this.props.img}/>
+                <h6 style={titleStyle}>{this.props.title}</h6>
+                <p style={descStyle}>{this.props.desc}</p>
+            </div>
+        )
+    }
+}
+
+export default CExperiences;
---
+++ client/components/CSectors.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+const styles = {
+    sectorStyle: {
+        backgroundColor: '#1F2022',
+        borderRadius: 25,
+        height: 240,
+    },
+    titleStyle: {
+        marginTop: 80,
+        marginLeft: 20,
+        fontWeight: 300,
+        textAlign: 'left',
+    } 
+}
+
+class CSectors extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div style={styles.sectorStyle}>
+                <img src={this.props.img}/>
+                <h5 style={styles.titleStyle}>{this.props.title}</h5>
+            </div>
+        )
+    }
+}
+
+export default CSectors;
