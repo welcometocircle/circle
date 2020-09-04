3173265154e61f19fdd59eb62d261edeccebed57
matrix303 <maitray303@gmail.com>
2020-09-04 12:55:11 -0400

    project hub started

---
+++ client/components/CProjectCard.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+class CProjectCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {}
+
+  render() {
+    const styles = {
+      card: {
+        backgroundColor: "#1F2022",
+        padding: 20,
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 10,
+        marginBottom: 10,
+        borderRadius: 40,
+        overflow: "hidden",
+      },
+      headImg: {
+        borderRadius: 30,
+      },
+      header: {},
+      title: {
+        textAlign: "left",
+      },
+      btnMore: {
+        background: "none",
+        border: "none",
+        // backgroundColor: "#C9C9C9",
+        // borderRadius: 50,
+        // textAlign: 'right'
+      },
+      btnImgMore: {
+        width: "50%",
+        height: "50%",
+      },
+    };
+    return (
+      <div style={this.props.style}>
+        <Col sm={6}>
+        <Card style={styles.card}>
+          <Card.Img src={this.props.headImg} style={styles.headImg} />
+          <Card.Title>
+            <Row>
+              <h5 style={styles.title}>{this.props.title}</h5>
+              <button style={styles.btnMore}>
+                {/* <h5>+</h5> */}
+                <img
+                  style={styles.btnImgMore}
+                  src="../assets/openCardsMore.png"
+                  className="float-right"
+                  data-target="#"
+                />
+              </button>
+            </Row>
+          </Card.Title>
+          <Collapse>
+            
+          </Collapse>
+          <Card.Body>
+            <p>{this.props.desc}</p>
+          </Card.Body>
+        </Card>
+        </Col>
+      </div>
+    );
+  }
+}
+
+export default CProjectCard;
--- client/components/POpenCircle.js
+++ client/components/POpenCircle.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row,Col } from "react-bootstrap";
+import CProjectCard from "./CProjectCard";
 
 class POpenCircle extends Component {
   constructor(props) {
@@ -15,13 +16,118 @@ class POpenCircle extends Component {
   componentWillUnmount() {}
 
   render() {
-		const styles = {};
+		const styles = {
+      header: {
+        marginTop: "25vh",
+        textAlign: "center",
+      },
+      headTxt: {
+        marginRight: "10vw",
+        marginLeft: "10vw",
+        marginTop: 30,
+      },
+      BGRight: {
+        position: "absolute",
+        float: "right",
+      },
+      BG: {
+        // backgroundImage: 'url("../assets/openBGRight.png")',
+        backgroungPosition: "right center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "",
+
+        // position: 'absolute',
+      },
+      HIW: {
+        backgroundColor: "#1F1F1F",
+        padding: 30,
+        borderRadius: 40,
+        marginTop:100,
+        textAlign: 'left',
+      },
+      HIWTitle: {
+
+      },
+      AP: {
+        marginTop:100
+      },
+    };
     return (
-      <Container>
-        <Row className="justify-content-center">
+      <div style={styles.BG}>
+        <Container style={styles.header}>
+          <h6>PROJECT HUB</h6>
           <h1>Open Circle</h1>
-        </Row>
-      </Container>
+          <p style={styles.headTxt}>
+            This is your chance to collaborate with other like-minded
+            individuals on projects that interest you. You bring the skills, we
+            coordinate the process. Select from current available projects or
+            submit an idea of your own.
+          </p>
+          {/* <Row className="justify-content-center" style={styles.header}> */}
+          {/* </Row> */}
+          <div style={styles.HIW}>
+            <Row>
+              <h5 style={styles.HIWTitle}>How it Works</h5>
+            </Row>
+            <Row>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum facilisis purus ac arcu malesuada, nec egestas dolor
+                sollicitudin.Description of the project goes here. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Vestibulum
+                facilisis purus ac arcu malesuada, nec egestas dolor
+                sollicitudin.
+              </p>
+            </Row>
+          </div>
+
+          <div style={styles.AP} >
+            <Row>
+              <h4 className="pull-left">Available Projects</h4>
+            </Row>
+            {/* <Row> */}
+              {/* <Col sm={6}> */}
+              <CProjectCard
+                headImg = '../assets/teamDesign.png'
+                title="Project Title"
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum facilisis purus ac arcu malesuada, nec egestas dolor
+                sollicitudin.Description of the project goes here. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Vestibulum
+                facilisis purus ac arcu malesuada, nec egestas dolor
+                sollicitudin."
+                sectors={["Busi", "Tech", "SSci", "Sci"]}
+                elegibility={[
+                  "Eligibility 1",
+                  "Eligibility 2",
+                  "Eligibility 3",
+                  "Eligibility 4",
+                ]}
+              />
+              {/* </Col> */}
+              {/* <Col sm={6} > */}
+              <CProjectCard
+                headImg = '../assets/teamEvent.png'
+                title="Project Title2"
+                desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum facilisis purus ac arcu malesuada, nec egestas dolor
+                sollicitudin.Description of the project goes here. Lorem ipsum
+                dolor sit amet, consectetur adipiscing elit. Vestibulum
+                facilisis purus ac arcu malesuada, nec egestas dolor
+                sollicitudin."
+                sectors={["Busi", "Tech", "SSci", "Sci"]}
+                elegibility={[
+                  "Eligibility 1",
+                  "Eligibility 2",
+                  "Eligibility 3",
+                  "Eligibility 4",
+                ]}
+              />
+              {/* </Col> */}
+            {/* </Row> */}
+          </div>
+        </Container>
+      </div>
     );
   }
 }
